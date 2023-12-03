aae0410143efb7cb9cc22c4cc063042a6f064c1a
Abhinav Asthana <143867960+asthana-16@users.noreply.github.com>
2023-12-03 18:59:38 +0530

    Add files via upload
    Python Code for Class and object.

---
+++ class.py
@@ -0,0 +1,20 @@
+
+class Student:
+    pass
+
+Stud_1 = Student()
+Stud_1.name = input("Enter Name: ")
+Stud_1.age = int(input("Enter age: "))
+Stud_1.degree = input("Enter degree: ")
+
+Stud_2= Student()
+Stud_2.name = input("Enter Name: ")
+Stud_2.age = int(input("Enter age: "))
+Stud_2.degree = input("Enter degree: ")
+
+print("Stud_1.name:",Stud_1.name)
+print("Stud_1.age:",Stud_1.age)
+print("Stud_1.degree:",Stud_1.degree)
+print("Stud_2.name:",Stud_2.name)
+print("Stud_2.age:",Stud_2.age)
+print("Stud_2.degree:",Stud_2.degree)
